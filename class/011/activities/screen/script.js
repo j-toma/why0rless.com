157ca1ec8175b82d20af4bb19873a2fdb52d49e8
jtoma <j.toma05@gmail.com>
2017-02-27 17:05:58 +0800

    excel screen work

--- class/011/activities/screen/script.js
+++ class/011/activities/screen/script.js
@@ -1,27 +1,31 @@
 var data = {
   1: {
+    key: 1,
     label: "Ribbon",
     question: "screen1",
     answer: "ascreen1"
   },
   2: {
+    key: 2,
     label: "Tab Row",
     question: "screen2",
     answer: "ascreen2"
   },
   3: {
+    key: 3,
     label: "Title Bar",
     question: "screen3",
     answer: "ascreen3"
   },
   4: {
+    key: 4,
     label: "Minimize, Restore, Close buttons",
     question: "screen4",
     answer: "ascreen4"
   }
 }
 
-window.onload = function () {
+function timer () {
 
   var seconds = 00;
   var tens = 00;
@@ -31,15 +35,15 @@ window.onload = function () {
   var appendSeconds = document.getElementById("seconds")
   var appendMinutes = document.getElementById("minutes")
   var appendHours = document.getElementById("hours")
-  var buttonStart = document.getElementById('button-start');
+  // var buttonStart = document.getElementById('button-start');
   var buttonStop = document.getElementById('button-stop');
   var buttonReset = document.getElementById('button-reset');
-  var Interval ;
+  var Interval;
 
-  buttonStart.onclick = function() {
-     clearInterval(Interval);
-     Interval = setInterval(startTimer, 10);
-  }
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+  // need to hide start button
+
   buttonStop.onclick = function() {
      clearInterval(Interval);
   }
@@ -52,7 +56,6 @@ window.onload = function () {
     appendTens.innerHTML = tens;
   	appendSeconds.innerHTML = seconds;
   }
-
   function startTimer () {
     tens++;
     if (tens < 9) {appendTens.innerHTML = "0" + tens;}
@@ -89,21 +92,59 @@ function shuffle(array) {
   return array;
 }
 
-function genPicOrder() {
-  var picOrder = shuffle(Object.keys(data));
-  console.log(picOrder);
-}
-
 function genPic(i) {
-  var div = document.getElementById('pictureContainer');
-  div.innerHTML = '<img src="img/screen' + i + '.jpg" style="width:1000px; height=700px;">'
+  var str = '<img src="img/screen' + i + '.jpg" style="width:1000px; height=700px;">'
+  return str
 }
 
 function genAnsPic(i) {
-  var div = document.getElementById('pictureContainer');
-  div.innerHTML = '<img src="img/screen' + i + '.jpg" style="width:1000px; height=700px;">'
+  console.log('genaspic here');
+  var str = '<img src="img/ascreen' + i + '.jpg" style="width:1000px; height=700px;">'
+  return str
 }
 
-function genList () {
+function genLabels () {
+  var labelOrder = shuffle(Object.keys(data));
+  var labels = document.getElementById('labels');
+  for (var i=0;i<labelOrder.length;i++) {
+    var label = document.createElement('input');
+    label.setAttribute('type','button');
+    label.setAttribute('id', 'label');
+    label.setAttribute('value', data[labelOrder[i]]['label']);
+    label.setAttribute('data-key', data[labelOrder[i]]['key']);
+    label.onclick = function () { handleClick(this) };
+    labels.appendChild(label);
+  }
+}
 
+function handleClick(input) {
+  console.log('current', current);
+  console.log('input dataset key', input.dataset.key);
+  if (input.dataset.key === current) {
+    document.getElementById('pictureContainer').innerHTML = genAnsPic(input.dataset.key);
+    // alert('donkey  balls');
+    // next();
+    document.getElementById('button-continue').style.visibility = "visible";
+  }
+}
+
+var picOrder = shuffle(Object.keys(data));
+var current = picOrder[-1];
+function game () {
+  document.getElementById('button-start').style.visibility = "hidden";
+  timer();
+  genLabels();
+  next();
+}
+
+function next () {
+  if (picOrder.length > 0) {
+    current = picOrder.pop();
+    console.log(current);
+    document.getElementById('pictureContainer').innerHTML = genPic(current);
+    document.getElementById('button-continue').style.visibility = "hidden";
+  }
+  else {
+    alert('you win');
+  }
 }
