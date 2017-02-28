8ef07e0631310345348e0eab80558aa8313b32ba
jtoma <j.toma05@gmail.com>
2017-02-28 15:23:59 +0800

    more pictures

--- class/011/activities/screen/script.js
+++ class/011/activities/screen/script.js
@@ -1,7 +1,7 @@
 var data = {
   1: {
     key: 1,
-    label: "Ribbon",
+    label: "Quick Access Toolbar",
     question: "screen1",
     answer: "ascreen1"
   },
@@ -97,12 +97,14 @@ function shuffle(array) {
 }
 
 function genPic(i) {
+  ansPage = false;
   var str = '<img src="img/screen' + i + '.jpg" style="width:100%";>'
   return str
 }
 
+var ansPage = false;
 function genAnsPic(i) {
-  console.log('genaspic here');
+  ansPage = true;
   var str = '<img src="img/ascreen' + i + '.jpg" style="width:100%; height=700px;">'
   return str
 }
@@ -112,24 +114,44 @@ function genLabels () {
   var labels = document.getElementById('labels');
   for (var i=0;i<labelOrder.length;i++) {
     var label = document.createElement('input');
+    label.setAttribute('class', 'button');
     label.setAttribute('type','button');
-    label.setAttribute('id', 'label');
+    var id = 'label' + data[labelOrder[i]]['key'];
+    label.setAttribute('id', id);
     label.setAttribute('value', data[labelOrder[i]]['label']);
     label.setAttribute('data-key', data[labelOrder[i]]['key']);
-    label.onclick = function () { handleClick(this) };
+    label.onclick = function () { if (!ansPage) { handleClick(this) } else { } };
     labels.appendChild(label);
   }
 }
 
+var wrong_answer_count = 0;
 function handleClick(input) {
-  console.log('current', current);
-  console.log('input dataset key', input.dataset.key);
   if (input.dataset.key === current) {
     document.getElementById('pictureContainer').innerHTML = genAnsPic(input.dataset.key);
     clearInterval(Interval);
     // alert('donkey  balls');
     // next();
+    document.getElementById('label'+input.dataset.key).style.visibility = "hidden";
     document.getElementById('button-continue').style.visibility = "visible";
+  }
+  else {
+    wrong_answer_count++;
+    if (wrong_answer_count === 10) {
+      alert('You seem to be having trouble. Please check page 6 of your book.');
+    }
+    else if (wrong_answer_count === 20) {
+      alert('Are you just guessing?');
+    }
+    else if (wrong_answer_count === 30) {
+      alert("Isn't your finger getting tired of randomly clicking? Go study!");
+    }
+    else if (wrong_answer_count === 40) {
+      alert("You have clicked incorrectly over 40 times. This could be a new record.")
+    }
+    else if (wrong_answer_count > 50) {
+      alert("I suggest you refresh the page.")
+    }
   }
 }
 
@@ -150,8 +172,10 @@ function next () {
     document.getElementById('button-continue').style.visibility = "hidden";
   }
   else {
-    console.log(minutes);
     var s = "<p>You win! You did it in " + minutes + " minutes " + "and " + seconds + " seconds.</p>";
-    document.getElementById('pictureContainer').innerHTML = s;
+    var t = "<p>You clicked incorrectly " + wrong_answer_count + " times.</p>";
+    document.getElementById('pictureContainer').innerHTML = s + t;
+    document.getElementById('button-continue').setAttribute('value', 'resart');
+    document.getElementById('button-continue').onclick = function () { location.reload()};
   }
 }
