b51671ebab4dbb250ba9aa0800289a67f0c24669
jtoma <j.toma05@gmail.com>
2017-03-03 16:17:05 +0800

    local upload successful

--- file_uploader.php
+++ file_uploader.php
@@ -1,21 +1,11 @@
 <?php
-
- $targetfolder = "testupload/";
-
- $targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
-
+$targetfolder = "testupload/";
+$targetfolder = $targetfolder . basename( $_FILES['file']['name']);
 if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
-
- {
-
- echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
-
- }
-
- else {
-
- echo "Problem uploading file";
-
- }
-
- ?>
+{
+  echo "The file ". basename( $FILES['file']['name']). " is uploaded";
+}
+else {
+  echo "Problem uploading file";
+}
+?>
