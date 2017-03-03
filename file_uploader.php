319d3853130ab17778d382f03a01724d314aefe2
jtoma <j.toma05@gmail.com>
2017-03-03 16:37:00 +0800

    phpT6 misses upload folder, suspect permission issue was last problem, fix is to mkdirectory

--- file_uploader.php
+++ file_uploader.php
@@ -1,5 +1,14 @@
 <?php
-$targetfolder = "testupload/";
+// $targetfolder = "testupload/";
+$targetfolder = 'upload';
+
+// create new directory with 744 permissions if it does not exist yet
+// owner will be the user/group the PHP script is run under
+if ( !file_exists($targetfolder) ) {
+   $oldmask = umask(0);  // helpful when used in linux server
+   mkdir ($targetfolder, 0744);
+}
+
 $targetfolder = $targetfolder . basename( $_FILES['file']['name']);
 if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
 {
