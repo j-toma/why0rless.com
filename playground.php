73b29151209266481bc7364d9be36ae1688b90b8
jtoma <j.toma05@gmail.com>
2017-03-03 15:48:04 +0800

    phpT3 w3school

--- playground.php
+++ playground.php
@@ -2,25 +2,11 @@
 <html>
 <body>
 
-  <form action="upload.php" method="post" enctype="multipart/form-data">
-      Select image to upload:
-      <input type="file" name="fileToUpload" id="fileToUpload">
-      <input type="submit" value="Upload Image" name="submit">
-  </form>
-
-<?php
-
-function myTest() {
-    // using x inside this function will generate an error
-    $x = 5 + 5;
-    echo "<p>Variable x inside function is: $x</p>";
-}
-myTest();
-
-echo "<p>Variable x outside function is: $x</p>";
-$txt = "W3Schools.com";
-echo "I love $txt!";
-?>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
 
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,11 +1,17 @@
 <?php
-$targetfolder = "upload/";
-$targetfolder = $targetfolder . basename($_FILES['file']['name']);
-if(move_uploaded_file($_FILES['file']['tmp_name'],$targetfolder))
-  {
-    echo "The file ". basename($_Files['file']['name']). " is uploaded";
-  }
-  else {
-    echo "There was a problem uplaoding the file";
-  }
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
 ?>
