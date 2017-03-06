efb20bf04e96696f152260b2b0ce1588bfc19ea7
jtoma <j.toma05@gmail.com>
2017-03-06 15:19:09 +0800

    basic pw work in mozilla

--- file_uploader.php
+++ file_uploader.php
@@ -1,21 +1,33 @@
+
+
 <?php
-// $targetfolder = "testupload/";
-$targetfolder = '/var/www/why0rless.com/uploads';
-// $targetpath = $targetfolder . basename( $_FILES['file']['name']);
+   if(isset($_FILES['file'])){
+      $errors= array();
+      $file_name = $_FILES['file']['name'];
+      $file_size =$_FILES['file']['size'];
+      $file_tmp =$_FILES['file']['tmp_name'];
+      $file_type=$_FILES['file']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['file']['name'])));
 
-// create new directory with 744 permissions if it does not exist yet
-// owner will be the user/group the PHP script is run under
-if ( !file_exists($targetfolder) ) {
-   $oldmask = umask(0);  // helpful when used in linux server
-   mkdir ($targetfolder, 0755);
-}
+      $extensions= array("jpeg","jpg","png","pdf");
 
-// $targetfolder = $targetfolder . basename( $_FILES['file']['name']);
-$name = basename( $_FILES['file']['name']);
-if(move_uploaded_file($_FILES['file']['tmp_name'], "$targetfolder/$name")) {
-  echo "The file ". basename( $FILES['file']['name']). " is uploaded";
-  // chmod("uploads/" . basename( $_FILES['file']['name']), 755);
-} else {
-  echo "Problem uploading file";
-}
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="Extension not allowed, please choose a .jpg, .jpeg, .png, or .pdf file.";
+      }
+
+      if($file_size > 10097152){
+         $errors[]='File size must be under 10MB';
+      }
+      $targetfolder = 'uploads';
+      $name = basename( $_FILES['file']['name']);
+
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"$targetfolder/$name");
+         echo "<ul><li>$file_name</li><li>$file_size</li><li>$file_type</li></ul>";
+
+      }else{
+        echo "Problem uploading file";
+        print_r($errors);
+      }
+   }
 ?>
